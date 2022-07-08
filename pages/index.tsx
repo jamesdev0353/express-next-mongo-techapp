52b34524b5cc43d5bb8812ca222922b900195d81
James <jamesdev0353@outlook.com>
2022-07-08 18:08:23 +0300

    cube is ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,62 @@
+import React from "react";
 import { useContext } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "./styles/Home.module.css";
 import { LoginContext } from "../client/components/Contexts";
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 
 export default function Home() {
   const context: any = useContext(LoginContext);
   const { userContextData, setUserContextData } = context;
+  const [rotate, setRotate] = React.useState(0);
+  const rotation = (number: number) => {
+    switch (number) {
+      case 0:
+        return `${styles.photoCube}`;
+      case 1:
+        return `${styles.photoCube} ${styles.rotate}`;
+      case 2:
+        return `${styles.photoCube} ${styles.rotate2}`;
+      case 3:
+        return `${styles.photoCube} ${styles.rotate3}`;
+      case 4:
+        return `${styles.photoCube} ${styles.rotate4}`;
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.cubeContainer}>
-        <div className={styles.photoCube}>
-          <div
-            // src="https://images.pexels.com/photos/274131/pexels-photo-274131.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            // alt=""
-            className={styles.front}
-          >
+        <div className={rotate ? rotation(rotate) : styles.photoCube}>
+          <div className={styles.front}>
             <span>This is A text</span>
+            <ArrowCircleRightIcon
+              className={styles.arrow}
+              onClick={() => setRotate(1)}
+            />
           </div>
 
-          <div
-            // src="https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            // alt=""
-            className={styles.right}
-          >
+          <div className={styles.right}>
             <span>Second Side</span>
+            <ArrowCircleRightIcon
+              className={styles.arrow}
+              onClick={() => setRotate(2)}
+            />
           </div>
-          <div
-            // src="https://images.pexels.com/photos/414860/pexels-photo-414860.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            // alt=""
-            className={styles.back}
-          >
+          <div className={styles.back}>
             <span>Third Side</span>
+            <ArrowCircleLeftIcon
+              className={styles.arrow}
+              onClick={() => setRotate(3)}
+            />
           </div>
           <div className={styles.left}>
             <h3>Rotate Cube</h3>
+            <ArrowCircleLeftIcon
+              className={styles.arrow}
+              onClick={() => setRotate(4)}
+            />
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi,
               quas?
