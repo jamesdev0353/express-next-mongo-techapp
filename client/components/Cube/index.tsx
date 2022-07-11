57ea083599d3255ffa762412fa2b65e196985bd3
James <jamesdev0353@outlook.com>
2022-07-11 17:38:17 +0300

    add image on the cube

--- client/components/Cube/index.tsx
+++ client/components/Cube/index.tsx
@@ -2,6 +2,22 @@ import React from "react";
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 import styles from "./Cube.module.scss";
+// import me from "./../assets/me.jpg";
+import Image from "next/image";
+import Avatar from "@mui/material/Avatar";
+
+// import Image from 'next/image'
+// import mypic from "./../assets/png.jpg";
+const MyImage = (props) => {
+  return (
+    <Image
+      src="/png.png"
+      alt="Picture of the author"
+      width="350px"
+      height="300px"
+    />
+  );
+};
 function Cube() {
   const [rotate, setRotate] = React.useState(0);
   const rotation = (number: number) => {
@@ -37,11 +53,11 @@ function Cube() {
           />
         </div>
         <div className={styles.back}>
-          <span>Third Side</span>
           <ArrowCircleLeftIcon
             className={styles.arrow}
             onClick={() => setRotate(3)}
           />
+          {/* <MyImage /> */}
         </div>
         <div className={styles.left}>
           <h3>Rotate Cube</h3>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ import dynamic from "next/dynamic";
 import styles from "./styles/Home.module.css";
 import { LoginContext } from "../client/components/Contexts";
 import Cube from "../client/components/Cube";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MailIcon from "@mui/icons-material/Mail";
 
 export default function Home() {
   const context: any = useContext(LoginContext);
@@ -19,18 +22,34 @@ export default function Home() {
       <div className={styles.mainDiv}>
         <div className={styles.detailsInfo}>
           <span>
-            whoami: Full Stack Node.js Engineer from Greece passionate with
+            whoami:
+            <br /> Full Stack Node.js Engineer from Greece passionate with
             coding and algorithms. With critical and analytical skills in
-            solving problems, fast learning and in depth, tracking new
-            technologies to bring the best effort in the team and in product.
-            Able to colaborate with a team or work as a unit and with time
-            pressure and deadlines.
+            solving problems, fast learning in depth, tracking new technologies
+            to bring the best effort in the team and in product. Able to
+            colaborate with a team or work as a unit, with time pressure and
+            deadlines.
             <br /> I like challenges because challenges make us better and
             better over time
           </span>
           <hr />
           <span>
-            utopia: Technologies and algorithms will not only help our lives run
+            studies:
+            <br /> 2021 - Now (Greece - Athens) MSc, in Computer Science at
+            University of Piraeus, Integrated Master
+            <br /> 2020 (Greece – Athens) MSc, Soil Science & Agricultural
+            Chemistry, Agricultural University of Athens, Integrated Master
+            <br />
+            2013 - 2020 (Greece– Athens) Natural Resources Management &
+            Agricultural Engineering, Agricultural University of Athens,
+            Bachelor
+            <br /> 2019 (Spain - Gijon) Capacity building on Creating, Designing
+            and Managing Innovative Ideas (TEAM LEADER), Erasmus +
+          </span>
+          <hr />
+          <span>
+            utopia:
+            <br /> Technologies and algorithms will not only help our lives run
             smoothly, will help us improve our civilization and expand life on
             earth or maybe on a diffrent planets.
             <br /> So is it our duty to help tech improve ?
@@ -39,6 +58,31 @@ export default function Home() {
         <div className={styles.cubeInfo}>
           <Cube />
         </div>
+      </div>
+      <div className={styles.mainDiv}>
+        <div className={styles.detailsInfo}>
+          <span>
+            Technologies:
+            <br />• Languages: Typescript. Javascript
+            <br />• Libraries: Node.js, React, Remix.js, Next.js, Express.js,
+            React Query, Redux, eslint, husky, JQuery.
+            <br />• Styling: CSS, SCSS, Material UI/ MUI, Tailwind CSS,
+            Bootstrap
+            <br />• Versioning tool: Git
+            <br />• Versioning platforms: Github, Gitlab build with
+            <br />• Testing: Jest enzyme, React test
+            <br />• Databases: MongoDB, SQL, Firebase
+            <br />• DevOps tools: LINUX, AWS cloud, VM’s, Docker
+            <br />• PaaS: Heroku, Netlify
+            <br />• Methodologies: agile, scrum
+          </span>
+          <hr />
+          <span>
+            Other Programming Knowledge:
+            <br />• Languages: JAVA, Python, Shell Scripting, C#
+          </span>
+        </div>
+        <div className={styles.cubeInfo}></div>
       </div>
     </div>
   );
